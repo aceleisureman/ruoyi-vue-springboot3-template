c7c3260ddc231f69bc489e9d3811ff7578c50aec
leisureman <ldlres@qq.com>
2025-07-16 19:02:56 +0800

    修复MapperScan配置冲突，解决factoryBeanObjectType错误

--- xinletu-admin/src/main/java/com/xinletu/RuoYiApplication.java
+++ xinletu-admin/src/main/java/com/xinletu/RuoYiApplication.java
@@ -1,6 +1,5 @@
 package com.xinletu;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author ruoyi
  */
 @SpringBootApplication
-@MapperScan("com.xinletu.**.mapper")
 public class RuoYiApplication
 {
     public static void main(String[] args)
--- xinletu-framework/src/main/java/com/xinletu/framework/config/ApplicationConfig.java
+++ xinletu-framework/src/main/java/com/xinletu/framework/config/ApplicationConfig.java
@@ -1,6 +1,7 @@
 package com.xinletu.framework.config;
 
 import java.util.TimeZone;
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
@@ -16,7 +17,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 // 表示通过aop框架暴露该代理对象,AopContext能够访问
 @EnableAspectJAutoProxy(exposeProxy = true)
 // 指定要扫描的Mapper类的包的路径
-@MapperScan("com.xinletu.**.mapper")
+@MapperScan({"com.xinletu.**.mapper"})
 public class ApplicationConfig
 {
     /**
